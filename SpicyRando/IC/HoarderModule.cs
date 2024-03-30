798b0c97f5fb212fc7b05f8760ec871e5fe50091
dplochcoder <daniel.ploch.coder@gmail.com>
2024-03-30 14:06:50 -0400

    Hoarder progress

--- SpicyRando/IC/HoarderModule.cs
+++ SpicyRando/IC/HoarderModule.cs
@@ -1,12 +1,371 @@
-﻿using ItemChanger;
+﻿using HutongGames.PlayMaker;
+using HutongGames.PlayMaker.Actions;
+using ItemChanger;
+using ItemChanger.Extensions;
+using ItemChanger.FsmStateActions;
+using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
 
 namespace SpicyRando.IC;
 
+internal class JarSpawnAdjuster : MonoBehaviour
+{
+    internal record JarSpawn
+    {
+        internal Func<GameObject> spawner;
+        internal int hp;
+        internal float yBump = 0;
+        internal float yVelBump = 0;
+        internal float rXVelBump = 0;
+        internal Action<GameObject>? customHook;
+
+        internal void Apply(FsmState state, HoarderModule mod)
+        {
+            var prefab = spawner.Invoke();
+            mod.SetPostSpawnHook(prefab, PostSpawnHooks);
+
+            var action = state.GetFirstActionOfType<SetSpawnJarContents>();
+            action.enemyPrefab.Value = spawner.Invoke();
+            action.enemyHealth.Value = hp;
+        }
+
+        private void PostSpawnHooks(GameObject obj)
+        {
+            if (yBump != 0) obj.transform.SetPositionY(obj.transform.position.y + yBump);
+            if (yVelBump != 0 || rXVelBump != 0)
+            {
+                var r2d = obj.GetComponent<Rigidbody2D>();
+                r2d.velocity += new Vector2(rXVelBump * UnityEngine.Random.Range(-1f, 1f), yVelBump);
+            }
+            customHook?.Invoke(obj);
+        }
+    }
+
+    internal record JarSpawnThreshold
+    {
+        internal int hpMin;
+        internal JarSpawn spawn1;
+        internal JarSpawn spawn2;
+        internal JarSpawn spawn3;
+
+        internal void Apply(PlayMakerFSM fsm, HoarderModule mod)
+        {
+            spawn1.Apply(fsm.GetState("Buzzer"), mod);
+            spawn2.Apply(fsm.GetState("Spitter"), mod);
+            spawn3.Apply(fsm.GetState("Roller"), mod);
+        }
+    }
+
+    internal static List<JarSpawnThreshold> SpawnLists()
+    {
+        return new()
+        {
+            new()
+            {
+                hpMin = 1600,
+                spawn1 = new()
+                {
+                    spawner = () => Preloader.Instance.Aspid,
+                    hp = 15,
+                },
+                spawn2 = new()
+                {
+                    spawner = () => Preloader.Instance.Baldur,
+                    hp = 15,
+                },
+                spawn3 = new()
+                {
+                    spawner = () => Preloader.Instance.Squit,
+                    hp = 8,
+                }
+            },
+            new()
+            {
+                hpMin = 1200,
+                spawn1 = new()
+                {
+                    spawner = Preloader.Instance.ArmoredSquitCage.ExtractFromCage,
+                    hp = 35,
+                    yBump = 0.3f,
+                    yVelBump = 1,
+                },
+                spawn2 = new()
+                {
+                    spawner = Preloader.Instance.ArmoredBaldurCage.ExtractFromCage,
+                    hp = 35,
+                    yBump = 0.1f,
+                },
+                spawn3 = new()
+                {
+                    spawner = Preloader.Instance.BattleObbleCage.ExtractFromCage,
+                    hp = 35,
+                    yBump = 0.5f,
+                    yVelBump = 2,
+                    rXVelBump = 2,
+                }
+            },
+            new()
+            {
+                hpMin = 800,
+                spawn1 = new()
+                {
+                    spawner = Preloader.Instance.PrimalAspidCage.ExtractFromCage,
+                    hp = 52,
+                    yBump = 0.25f,
+                    yVelBump = 2,
+                },
+                spawn2 = new()
+                {
+                    spawner = () => Preloader.Instance.FlukeFey,
+                    hp = 52,
+                    yBump = 0.5f,
+                    yVelBump = 1,
+                    customHook = AdjustFlukeFey,
+                },
+                spawn3 = new()
+                {
+                    spawner = () => Preloader.Instance.GreatHopper,
+                    hp = 80,
+                    yBump = 1.5f,
+                }
+            },
+            new()
+            {
+                hpMin = 400,
+                spawn1 = new()
+                {
+                    spawner = () => Preloader.Instance.BroodingMawlek,
+                    hp = 78,
+                    yBump = 0.5f,
+                    customHook = AdjustMawlek,
+                },
+                spawn2 = new()
+                {
+                    spawner = () => Preloader.Instance.WingedNosk,
+                    hp = 78,
+                    yBump = 1.5f,
+                },
+                spawn3 = new()
+                {
+                    spawner = () => Preloader.Instance.GodTamerBeast,
+                    hp = 78,
+                    yBump = 2.5f,
+                }
+            },
+            new()
+            {
+                hpMin = 0,
+                spawn1 = new()
+                {
+                    spawner = () => Preloader.Instance.Oblobble,
+                    hp = 100,
+                },
+                spawn2 = new()
+                {
+                    spawner = () => Preloader.Instance.Marmu,
+                    hp = 100,
+                },
+                spawn3 = new()
+                {
+                    spawner = () => Preloader.Instance.WatcherKnight,
+                    hp = 100,
+                }
+            },
+        };
+    }
+
+    private List<JarSpawnThreshold> thresholds = SpawnLists();
+    private HoarderModule? mod;
+
+    private HealthManager healthManager;
+    private PlayMakerFSM collectorFsm;
+    private JarSpawnThreshold? currentThreshold;
+
+    internal void SetMod(HoarderModule module) => mod = module;
+
+    private void Awake()
+    {
+        healthManager = GetComponent<HealthManager>();
+        collectorFsm = gameObject.LocateMyFSM("Control");
+    }
+
+    private JarSpawnThreshold? GetCurrentThreshold() => thresholds.Where(t => t.hpMin <= healthManager.hp).FirstOrDefault();
+
+    private void Update()
+    {
+        var next = GetCurrentThreshold();
+        if (next != null && next.hpMin != (currentThreshold?.hpMin ?? -1))
+        {
+            currentThreshold = next;
+            currentThreshold.Apply(collectorFsm, mod);
+        }
+    }
+
+    private const float X1 = 40;
+    private const float X2 = 70;
+
+    private static void AdjustFlukeFey(GameObject obj) => obj.LocateMyFSM("Fluke Fly").FsmVariables.GetFsmBool("FLUKE MOTHER").Value = true;
+
+    private const float MAWLEK_SCALE = 0.8f;
+    private const float MAWLEK_BUFFER = 6;
+    private const float MAWLEK_JUMP_DAMPENER = 0.6f;
+
+    private static void AdjustMawlek(GameObject obj)
+    {
+        obj.transform.localScale = new(MAWLEK_SCALE, MAWLEK_SCALE, MAWLEK_SCALE);
+
+        var fsm = obj.LocateMyFSM("Mawlek Control");
+
+        var init = fsm.GetState("Init");
+        init.RemoveFirstActionOfType<NextFrameEvent>();
+        init.AddLastAction(new Lambda(() => fsm.SetState("Wake In Air")));
+
+        var wakeLand = fsm.GetState("Wake Land");
+        wakeLand.AddLastAction(new Lambda(() =>
+        {
+            fsm.FsmVariables.GetFsmFloat("Start X").Value = Mid(obj.transform.position.x, X1 + MAWLEK_BUFFER, X2 - MAWLEK_BUFFER);
+            fsm.SetState("Start");
+        }));
+
+        var r2d = obj.GetComponent<Rigidbody2D>();
+        AdjustMawlekJump(r2d, fsm.GetState("Jump"));
+        AdjustMawlekJump(r2d, fsm.GetState("Jump 2"));
+    }
+
+    private static void AdjustMawlekJump(Rigidbody2D r2d, FsmState state)
+    {
+        var setVel = state.GetFirstActionOfType<SetVelocity2d>();
+        int index = state.Actions.IndexOf(setVel) + 1;
+        state.InsertAction(new Lambda(() => UpdateMawlekJumpVelocity(r2d)), index);
+    }
+
+    private static void UpdateMawlekJumpVelocity(Rigidbody2D r2d)
+    {
+        var vel = r2d.velocity;
+        vel.x /= MAWLEK_JUMP_DAMPENER;
+        vel.y *= Mathf.Sqrt(MAWLEK_JUMP_DAMPENER);
+        r2d.velocity = vel;
+    }
+
+    private static float Mid(float a, float b, float c)
+    {
+        bool ab = a < b;
+        bool ac = a < c;
+        bool bc = b < c;
+
+        if (ab)
+        {
+            if (ac) return bc ? b : c;
+            else return a;
+        }
+        else
+        {
+            if (bc) return ac ? a : c;
+            else return b;
+        }
+    }
+}
+
 internal class HoarderModule : ItemChanger.Modules.Module
 {
-    public override void Initialize() { }
+    private static readonly FsmID CONTROL = new("Jar Collector", "Control");
+    private static readonly FsmID PHASE_CONTROL = new("Jar Collector", "Phase Control");
+    private static readonly FsmID STUN_CONTROL = new("Jar Collector", "Stun Control");
 
-    public override void Unload() { }
+    private const int COLLECTOR_HP = 800;  // 2000;
+    private const int PHASE_2_HP = 1000;
+
+    private ILHook? spawnHook;
+
+    public override void Initialize()
+    {
+        Events.AddFsmEdit(CONTROL, BuffCollectorSpawns);
+        Events.AddFsmEdit(PHASE_CONTROL, EliminatePhaseControl);
+        Events.AddFsmEdit(STUN_CONTROL, EliminateStunControl);
+        spawnHook = new(typeof(SpawnJarControl).GetMethod("Behaviour", BindingFlags.Instance | BindingFlags.NonPublic).GetStateMachineTarget(), FixJarSpawn);
+    }
+
+    public override void Unload()
+    {
+        Events.RemoveFsmEdit(CONTROL, BuffCollectorSpawns);
+        Events.RemoveFsmEdit(PHASE_CONTROL, EliminatePhaseControl);
+        Events.RemoveFsmEdit(STUN_CONTROL, EliminateStunControl);
+        spawnHook?.Dispose();
+    }
+
+    private void BuffCollectorSpawns(PlayMakerFSM fsm)
+    {
+        // Adapt hp.
+        var healthManager = fsm.gameObject.GetComponent<HealthManager>();
+        fsm.GetState("Start Fall").AddFirstAction(new Lambda(() => healthManager.hp = COLLECTOR_HP));
+
+        // Adjust jar spawns.
+        fsm.gameObject.AddComponent<JarSpawnAdjuster>().SetMod(this);
+    }
+
+    private void EliminatePhaseControl(PlayMakerFSM fsm)
+    {
+        fsm.GetState("Init").AddLastAction(new Lambda(() =>
+        {
+            var vars = fsm.gameObject.LocateMyFSM("Control").FsmVariables;
+            vars.GetFsmInt("Spawn Min").Value = 2;
+            vars.GetFsmInt("Spawn Max").Value = 3;
+        }));
+
+        fsm.GetState("Check").GetFirstActionOfType<IntCompare>().integer2 = new FsmInt(PHASE_2_HP);
+
+        var phase2 = fsm.GetState("Phase 2");
+        while (phase2.Actions.Length > 0) phase2.RemoveAction(0);
+
+        phase2.AddLastAction(new Lambda(() =>
+        {
+            var vars = fsm.gameObject.LocateMyFSM("Control").FsmVariables;
+            vars.GetFsmInt("Enemies Max").Value = 12;
+            vars.GetFsmInt("Spawn Min").Value = 3;
+            vars.GetFsmInt("Spawn Max").Value = 4;
+            vars.GetFsmFloat("Resummon Pause").Value = 0.15f;
+            vars.GetFsmFloat("Spawn Recover Time").Value = 0.55f;
+            vars.GetFsmFloat("Hop X Speed").Value = -14;
+        }));
+    }
+
+    private void EliminateStunControl(PlayMakerFSM fsm)
+    {
+        var vars = fsm.FsmVariables;
+        vars.FindFsmInt("Stun Combo").Value = 999;
+        vars.FindFsmInt("Stun Hit Max").Value = 999;
+    }
+
+    private void FixJarSpawn(ILContext il)
+    {
+        ILCursor cursor = new(il);
+        cursor.Goto(0);
+        cursor.GotoNext(i => i.MatchLdfld<SpawnJarControl>("enemyToSpawn"));
+        cursor.GotoNext(i => i.MatchCall(typeof(ObjectPoolExtensions).FullName, nameof(ObjectPoolExtensions.Spawn)));
+        cursor.Remove();
+        cursor.EmitDelegate(CustomJarSpawn);
+    }
+
+    private Dictionary<GameObject, Action<GameObject>> postSpawnHooks = new();
+
+    internal void SetPostSpawnHook(GameObject prefab, Action<GameObject> hook) => postSpawnHooks[prefab] = hook;
+
+    private GameObject CustomJarSpawn(GameObject self, Vector3 position)
+    {
+        var obj = UnityEngine.Object.Instantiate(self);
+        obj.transform.position = position;
+
+        obj.SetActive(true);
+        if (postSpawnHooks.TryGetValue(self, out var hook)) hook.Invoke(obj);
+
+        return obj;
+    }
 }
 
 internal class HoarderFeature : SpicyFeature
--- SpicyRando/Preloader.cs
+++ SpicyRando/Preloader.cs
@@ -7,14 +7,26 @@ internal class Preloader : PurenailCore.ModUtil.Preloader
 {
     public static Preloader Instance { get; private set; } = new();
 
-    [Preload("Waterways_08", "Ceiling Dropper")]
-    public GameObject Belfly { get; private set; }
+    [Preload("Room_Colosseum_Gold", "Colosseum Manager/Waves/Wave 4/Colosseum Cage Small")]
+    public GameObject ArmoredBaldurCage { get; private set; }
+
+    [Preload("Room_Colosseum_Gold", "Colosseum Manager/Waves/Wave 12/Colosseum Cage Small (3)")]
+    public GameObject ArmoredSquitCage { get; private set; }
+
+    [Preload("Crossroads_ShamanTemple", "_Enemies/Roller 6")]
+    public GameObject Baldur { get; private set; }
 
     [Preload("Fungus3_23", "Battle Gate")]
     public GameObject BattleGateHorizontal { get; private set; }
 
     [Preload("Fungus3_23_boss", "Battle Scene/Battle Gate (2)")]
     public GameObject BattleGateVertical { get; private set; }
+
+    [Preload("Room_Colosseum_Silver", "Colosseum Manager/Waves/Wave 26 Obble/Colosseum Cage Small (1)")]
+    public GameObject BattleObbleCage { get; private set; }
+
+    [Preload("Waterways_08", "Ceiling Dropper")]
+    public GameObject Belfly { get; private set; }
 
     [Preload("Crossroads_09", "_Enemies/Mawlek Body")]
     public GameObject BroodingMawlek { get; private set; }
@@ -25,8 +37,20 @@ internal class Preloader : PurenailCore.ModUtil.Preloader
     [Preload("Fungus2_23", "Cave Spikes tile")]
     public GameObject CaveSpikesTile { get; private set; }
 
+    [Preload("Waterways_02", "Fluke Fly (1)")]
+    public GameObject FlukeFey { get; private set; }
+
+    [Preload("Room_Colosseum_Gold", "Colosseum Manager/Waves/Lobster Lancer/Entry Object/Lobster")]
+    public GameObject GodTamerBeast { get; private set; }
+
+    [Preload("Deepnest_East_06", "Giant Hopper (1)")]
+    public GameObject GreatHopper { get; private set; }
+
     [Preload("Hive_01", "Hive Breakable Pillar (1)")]
     public GameObject HiveWall { get; private set; }
+
+    [Preload("Fungus3_40_boss", "Warrior/Ghost Warrior Marmu")]
+    public GameObject Marmu { get; private set; }
 
     [Preload("Fungus2_03", "Mushroom Turret (4)")]
     public GameObject MushroomTurretCeiling { get; private set; }
@@ -37,6 +61,9 @@ internal class Preloader : PurenailCore.ModUtil.Preloader
     [Preload("Fungus2_03", "Mushroom Turret")]
     public GameObject MushroomTurretRight { get; private set; }
 
+    [Preload("Room_Colosseum_Silver", "Colosseum Manager/Waves/Wave 30 Obble/Mega Fat Bee")]
+    public GameObject Oblobble { get; private set; }
+
     [Preload("Fungus3_48", "Plant Trap (8)")]
     public GameObject PlantTrapFloor { get; private set; }
 
@@ -46,5 +73,25 @@ internal class Preloader : PurenailCore.ModUtil.Preloader
     [Preload("Fungus3_48", "Plant Trap (6)")]
     public GameObject PlantTrapRight { get; private set; }
 
+    [Preload("Room_Colosseum_Gold", "Colosseum Manager/Waves/Wave 17/Colosseum Cage Small (2)")]
+    public GameObject PrimalAspidCage { get; private set; }
+
+    [Preload("Crossroads_19", "_Enemies/Spitter")]
+    public GameObject Aspid { get; private set; }
+
+    [Preload("Tutorial_01", "_Enemies/Buzzer 2")]
+    public GameObject Squit { get; private set; }
+
     public TinkEffect TinkEffect => CaveSpikesInvis.GetComponent<TinkEffect>();
+
+    [Preload("Ruins2_03_boss", "Battle Control/Black Knight 4")]
+    public GameObject WatcherKnight { get; private set; }
+
+    [Preload("GG_Nosk_Hornet", "Battle Scene/Hornet Nosk")]
+    public GameObject WingedNosk { get; private set; }
+}
+
+internal static class GameObjectExtensions
+{
+    internal static GameObject ExtractFromCage(this GameObject self) => self.LocateMyFSM("Spawn").Fsm.GetFsmGameObject("Enemy Type").Value;
 }
